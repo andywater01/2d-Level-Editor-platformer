3696ba3e9ff5cdc7f40b9a0ef547e45fb90dc311
Andy <andrew.waterhouse@ontariotechu.net>
2021-09-27 13:25:52 -0400

    Cleaned up code
    Removed unnecessary Start and Update functions

--- Assets/Spike.cs
+++ Assets/Spike.cs
@@ -6,17 +6,7 @@ public class Spike : MonoBehaviour
 {
     private Movement move;
     private GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
--- Assets/follow.cs
+++ Assets/follow.cs
@@ -7,11 +7,7 @@ public class follow : MonoBehaviour
     public GameObject Player;
     public GameObject Camera;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
